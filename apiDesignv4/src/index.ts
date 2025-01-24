bd3f8f7c79984dc55bbbd83bfebe9cc0125c61b5
Vinnie <35411050+avipami@users.noreply.github.com>
2025-01-24 14:16:50 +0100

    updated

--- apiDesignv4/src/index.ts
+++ apiDesignv4/src/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv'
+dotenv.config() // checks .env file and makes everything available on process
+
 import app from './server'
 
 app.listen(3001, () => {
--- apiDesignv4/src/server.ts
+++ apiDesignv4/src/server.ts
@@ -1,7 +1,17 @@
 import express from 'express'
 import router from './router'
-
+import morgan from 'morgan'
+import { protect } from './modules/auth'
+import { createNewUser, signin } from './handlers/user'
 const app = express()
+
+app.use(morgan('dev')) //middleware for console logging
+app.use(express.json()) // allows a client to send a json
+app.use(express.urlencoded({extended: true})) // this puts json into an object
+
+app.use((req, res, next) => {
+    next()
+})
 
 app.get('/', (req, res) => {
     console.log("hello there")
@@ -10,6 +20,9 @@ app.get('/', (req, res) => {
 })
 
 // the router exists after url/api/*
-app.use('/api', router)
+app.use('/api', protect, router) //protect is the middleware for jwt auth
+
+app.post('/user', createNewUser)
+app.post('/signin', signin)
 
 export default app
