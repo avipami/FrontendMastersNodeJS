cf174b2815df314cdd73083c547856dec7dff91c
Vinnie <35411050+avipami@users.noreply.github.com>
2025-05-09 11:38:12 +0200

    made until get product handlers

--- apiDesignv4/src/router.ts
+++ apiDesignv4/src/router.ts
@@ -1,4 +1,6 @@
 import {Router} from 'express'
+import {body, oneOf, validationResult} from 'express-validator'
+import { handleInputErrors } from './modules/middleware'
 
 const router = Router()
 
@@ -9,10 +11,15 @@ router.get('/product', (req, res) => {
 router.get('/product/:id', () => {})
 
 //update
-router.put('/product/:id', () => {})
+router.put('/product/:id', body('name').isString, handleInputErrors,(req, res) => {
+    
+
+})
 
 //create
-router.post('/product', () => {})
+router.post('/product', body('name').isString, handleInputErrors,(req, res) => {
+    
+})
 
 //delete
 router.delete('/product/:id', () => {})
@@ -25,13 +32,26 @@ router.get('/update', () => {})
 router.get('/update/:id', () => {})
 
 //update
-router.put('/update/:id', () => {})
+router.put('/update/:id', 
+    body('title').optional(), 
+    body('body').optional(), 
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+    body('version').optional(), 
+    () => {
+
+    }
+)
 
 //create
 router.post('/update', () => {})
 
 //delete
-router.delete('/update/:id', () => {})
+router.delete('/update/:id', 
+    body('title').exists().isString(), 
+    body('body').exists().isString(),
+    () => {
+
+    })
 
 /**
  * Update Points
@@ -41,10 +61,19 @@ router.get('/updatepoint', () => {})
 router.get('/updatepoint/:id', () => {})
 
 //update
-router.put('/updatepoint/:id', () => {})
+router.put('/updatepoint/:id', 
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+    () => {}
+)
 
 //create
-router.post('/updatepoint', () => {})
+router.post('/updatepoint',
+    body('name').isString(),
+    body('description').isString(),
+    body('updateId').exists().isString(),
+    () => {}
+)
 
 //delete
 router.delete('/updatepoint/:id', () => {})
